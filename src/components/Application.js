eeeeb17d04ddb6388e237c554118847cae0c83a3
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-09-30 12:06:25 -0700

    added proxy in package.json file

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,9 +14,9 @@ const appointments = [
     interview: {
       student: 'Lydia Miller-Jones',
       interviewer: {
-        id: 1,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
+        id: 2,
+        name: 'Tori Malcolm',
+        avatar: 'https://i.imgur.com/Nmx0Qxo.png',
       },
     },
   },
